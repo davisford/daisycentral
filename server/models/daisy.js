9a53af9225223a6a170d0dec4afeda8c336b2086
davisford <davisford@gmail.com>
2012-05-11 00:31:31 -0400

    more tests

--- server/models/daisy.js
+++ server/models/daisy.js
@@ -1,26 +1,26 @@
 // in server/models/daisy.js
 
 /*
-	{ "did" : "Troys-Mailbox", 
-	  "ip" : "192.168.25.132", 
-	  "mac" : "00:06:66:72:16:81", 
-	  "timestamp" : 1335246150000, 
-	  "bat" : 3069, 
-	  "rssi" : 230, 
-	  "wake" : 7, 
-	  "seq" : 81, 
-	  "PIO3" : 0, 
-	  "PIO2" : 0, 
-	  "PIO1" : 0, 
-	  "AD0" : 2237, 
-	  "AD1" : 2166, 
-	  "AD2" : 10, 
-	  "AD3" : 34803, 
-	  "AD4" : 3131, 
-	  "AD5" : 27265, 
-	  "AD6" : 12560, 
-	  "AD7" : 12574, 
-	  "_id" : ObjectId("4f963d422c6fab0000000008") 
+  { "did" : "Troys-Mailbox", 
+    "ip" : "192.168.25.132", 
+    "mac" : "00:06:66:72:16:81", 
+    "timestamp" : 1335246150000, 
+    "bat" : 3069, 
+    "rssi" : 230, 
+    "wake" : 7, 
+    "seq" : 81, 
+    "PIO3" : 0, 
+    "PIO2" : 0, 
+    "PIO1" : 0, 
+    "AD0" : 2237, 
+    "AD1" : 2166, 
+    "AD2" : 10, 
+    "AD3" : 34803, 
+    "AD4" : 3131, 
+    "AD5" : 27265, 
+    "AD6" : 12560, 
+    "AD7" : 12574, 
+    "_id" : ObjectId("4f963d422c6fab0000000008") 
  */
 
 var mongoose = require('mongoose')
@@ -57,18 +57,35 @@ var DaisySchema = new Schema({
 
 DaisySchema.virtual('localtime')
   .get(function (){
-  	var localNow = new Date().getTimezoneOffset(),
-  	    min = this.timestamp / 1000 / 60;
-  	return (min - localNow) * 1000 * 60;
+    var localNow = new Date().getTimezoneOffset(),
+        min = this.timestamp / 1000 / 60;
+    return (min - localNow) * 1000 * 60;
   });
 DaisySchema.virtual('date')
   .get(function (){
-  	return new Date(this.timestamp);
+    return new Date(this.timestamp);
   });
 DaisySchema.virtual('localdate')
   .get(function (){
-  	return new Date(this.localtime);
+    return new Date(this.localtime);
   });
 
-var Daisy = mongoose.model('Daisy', DaisySchema);
-module.exports.Daisy = Daisy;
+//var Daisy = mongoose.model('Daisy', DaisySchema);
+//module.exports.Daisy = Daisy;
+module.exports.DaisySchema = DaisySchema;
+var Daisy = function() {
+  var _getModel = function(collectionName) {
+    return mongoose.model(collectionName, DaisySchema, collectionName);
+  }
+  var _getColName = function(mac) {
+    if(typeof(mac) === "undefined" || mac === null || mac.length === 0) {
+      throw new Error("mac address cannot be null/empty")
+    }
+    return "sensors."+mac;
+  }
+  return {
+    getModel: _getModel,
+    getColName: _getColName
+  }
+}();
+module.exports = Daisy;
--- test/daisy-tests.js
+++ test/daisy-tests.js
@@ -1,38 +1,71 @@
 var should = require('should')
   , mongoose = require('mongoose')
-  , Daisy = require("../server/models/daisy").Daisy;
+  , DaisySchema = require('../server/models/daisy').DaisySchema;
 
 // this is a test
 process.env['NODE_ENV'] = 'test';
 
 mongoose.connect("mongodb://localhost/daisycentral-test");
 
+var Daisy = require('../server/models/daisy')
+  , colOne = Daisy.getColName("00:11:22:33:44:55")
+  , colTwo = Daisy.getColName("00:DE:AD:BE:EF:00")
+  , DaisyOne = Daisy.getModel(colOne)
+  , DaisyTwo = Daisy.getModel(colTwo);
+
+var dataOne = {
+			did: "Davis' Garage", ip: "192.168.25.132", mac: "00:06:66:72:16:81", timestamp: 1335246150000, bat: 3069, wake: 7, seq: 81,
+			PIO3: 0, PIO2: 1, PIO1: 0, AD0: 2237, AD1: 2166, AD2: 10, AD3: 34803, AD4: 3131, AD5: 27265, AD6: 12560, AD7: 12574 };
+var dataTwo = {
+			did: "Davis' Kitchen", ip: "192.168.25.132", mac: "00:06:77:88:99:AA", timestamp: 1335246150000, bat: 3069, wake: 7, seq: 81,
+			PIO3: 0, PIO2: 1, PIO1: 0, AD0: 2237, AD1: 2166, AD2: 10, AD3: 34803, AD4: 3131, AD5: 27265, AD6: 12560, AD7: 12574 };
+
 describe("Daisy", function() {
-	var currentDaisy = null;
 
 	beforeEach(function (done) {
-	  Daisy.remove({}, function (err) {
+	  DaisyOne.remove({}, function (err) {
       if (err) { return done(err); }
-      done();
-	  });
+      DaisyTwo.remove({}, function (err) {
+      	if (err) { return done(err); }
+      })
+	  }); 
+	  done();
 	});
 
 	afterEach(function (done) {
-	  Daisy.remove({}, function (err) {
+	  DaisyOne.remove({}, function (err) {
       if (err) { return done(err); }
-      done();
-	  });
+      DaisyTwo.remove({}, function (err) {
+      	if (err) { return done(err); }
+      })
+	  }); 
+	  done();
 	});
 
-	it("should save a daisy to the correct collection", function(done) {
-		var daisy = new Daisy({
-			did: "Davis' Garage", ip: "192.168.25.132", mac: "00:06:66:72:16:81", timestamp: 1335246150000, bat: 3069, wake: 7, seq: 81,
-			PIO3: 0, PIO2: 1, PIO1: 0, AD0: 2237, AD1: 2166, AD2: 10, AD3: 34803, AD4: 3131, AD5: 27265, AD6: 12560, AD7: 12574 });
+	it("should save sensors to collection named: "+colOne, function(done) {
+		var daisy = new DaisyOne(dataOne);
 		daisy.save(function (err, daisy) {
       if (err) { return done(err); }
       should.exist(daisy);
       daisy.should.have.property('_id');
-      done()
+      DaisyOne.findOne({'_id': daisy._id}, function(err, doc) {
+      	should.not.exist(err);
+      	should.exist(doc);
+      	doc.collection.name.should.eql(colOne);
+      	daisy._id.should.eql(doc._id);
+      	done();
+      });
 		});
+	});
+
+	it("should save sensors to collection named: "+colTwo, function(done) {
+		var daisy = new DaisyTwo(dataTwo);
+		daisy.save(function (err, doc) {
+			if (err) { return done(err); }
+			should.exist(doc);
+			doc.should.have.property('_id');
+			doc.collection.name.should.eql(colTwo);
+			done();
+		})
 	})
 })
