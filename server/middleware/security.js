884b3c6324d608dc89b93c3908cf65a00aaa80ba
davisford <davisford@gmail.com>
2012-08-25 13:05:39 -0400

    simplify middleware

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -2,45 +2,38 @@
 
 // if the user is authenticated, redirect them to /
 // else redirect them to /login
-exports.authenticated = function () {
+exports.authenticated = function (req, res, next) {
 
-  return function (req, res, next) {
-
-    if (req.session.userId) {
-      return next();
-    }
-    if (res.redirect) {
-      return res.redirect('/login');
-    } else {
-      return res(new Error('unauthorized'));
-    }
-  };
+  if (req.session.userId) {
+    return next();
+  }
+  if (res.redirect) {
+    return res.redirect('/login');
+  } else {
+    return res(new Error('unauthorized'));
+  }
 
 };
 
 // only let user through if they are admin
-exports.isAdmin = function () {
+exports.isAdmin = function (req, res, next) {
 
-  return function (req, res, next) {
-    if (req.session.isAdmin === true) {
-      return next();
-    } else {
-      throw new Error('unauthorized');
-    }
-  };
+  if (req.session.isAdmin === true) {
+    return next();
+  } else {
+    throw new Error('unauthorized');
+  }
 
 };
 
 // if the user is authenticated, redirect them to /
 // else let them pass through -- to be used for /login route only
-exports.validCookie = function () {
+exports.validCookie = function (req, res, next) {
 
-  return function (req, res, next) {
-    if (req.session.userId) {
-      return res.redirect('/');
-    }
-    return next();
-  };
+  if (req.session.userId) {
+    return res.redirect('/');
+  }
+  return next();
 
 };
 
